72d969f6ead7db7de65b56c81d3eafca16000cf5
taco <taco@taco.taco>
2024-08-15 23:12:20 +0300

    4.5.

--- tests/list_helpers.test.js
+++ tests/list_helpers.test.js
@@ -78,5 +78,81 @@ describe('total likes', () => {
   })
 })
 
+describe('Favorite blog', () => {
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5
+    },
+    {
+      _id: '5a422aa719999a676234d17f8',
+      title: 'Some other blog',
+      author: 'Wise person',
+      url: 'https://someblog.com',
+      likes: 45
+    },
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5
+    },
+    {
+      _id: '6a422aa719999a676234d17f8',
+      title: 'blogger',
+      author: 'Person',
+      url: 'https://someblog.com',
+      likes: 45
+    },
+    {
+      _id: '5a422aa719999a676234d17f8',
+      title: 'Some other blog',
+      author: 'Wise person',
+      url: 'https://someblog.com',
+      likes: 45
+    },
+  ]
+
+  const listWithNullValues = [
+
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 0
+    },
+    {
+      _id: '6a422aa719999a676234d17f8',
+      title: 'blogger',
+      author: 'Person',
+      url: 'https://someblog.com',
+      likes: 0
+    },
+  ]
+
+  test('When list contains two items, it should return the one with the most likes', () => {
+    const result = listHelper.favoriteBlog(listWithTwoBlogs)
+    assert.deepStrictEqual(result, {title: 'Some other blog', author: 'Wise person', likes: 45} )
+  })
+
+  test('When list contains many favorites, it should return the top one', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs)
+    assert.deepStrictEqual(result, {title: 'blogger', author: 'Person', likes: 45} )
+  })
+
+  test('When list has only zero values, it should return an error', () => {
+    const result = listHelper.favoriteBlog(listWithNullValues)
+    assert.deepStrictEqual(result, {error: "No favorites. Likes amount to zero"} )
+  })
+})
+
 
 
--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,3 +1,4 @@
+const logger = require('../utils/logger')
 const dummy = (blogs) => {
   return 1
 }
@@ -10,7 +11,20 @@ const totalLikes = (blogs) => {
   return total
 }
 
+const favoriteBlog = (blogs) => {
+  const reduce = blogs.reduce((mostLikes, blog) => {
+    return blog.likes > mostLikes.likes ? blog : mostLikes
+  }) 
+  logger.info(reduce)
+
+  if (reduce.likes === 0)
+    return {error: "No favorites. Likes amount to zero"}
+
+  return {title: reduce.title, author: reduce.author, likes: reduce.likes}
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
